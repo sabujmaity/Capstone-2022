7ce5148fccb2b9e61c42d1935d0d36c779272c78
Manas <Madanmanna123456@gmail.com>
2022-04-04 17:01:20 +0530

    added features section and some design changes

--- index.js
+++ index.js
@@ -31,3 +31,33 @@ fetch(
 
 //if error happens
     .catch((err) => console.error(err));
+
+
+//adding dark mode using jquery
+$('.switch').on('click',function(){
+    $('body').toggleClass("dark");
+})
+
+//adding features sliding function
+var slider=document.querySelector('.symtoms-slider');
+var active=document.querySelector('#active');
+var line1=document.querySelector('#line1');
+var line2=document.querySelector('#line2');
+var line3=document.querySelector('#line3');
+var line4=document.querySelector('#line4');
+line1.addEventListener('click',function(){
+    slider.style.transform='translateX(0)';
+    active.style.top='0px';
+})
+line2.addEventListener('click',function(){
+    slider.style.transform='translateX(-25%)';
+    active.style.top='80px';
+})
+line3.addEventListener('click',function(){
+    slider.style.transform='translateX(-50%)';
+    active.style.top='160px';
+})
+line4.addEventListener('click',function(){
+    slider.style.transform='translateX(-75%)';
+    active.style.top='240px';
+})
