071ad296fcb2ac5c8b6b954ce0979acb3389c093
Manas <Madanmanna123456@gmail.com>
2022-04-05 09:49:58 +0530

    some UI changes

--- index.js
+++ index.js
@@ -9,28 +9,28 @@ const options = {
         "X-RapidAPI-Key": "f065dae94dmsh9aa648e4262fcb5p1ca83ejsn37f3f7b18d6e",
     },
 };
-fetch(
-    "https://covid-19-news.p.rapidapi.com/v1/covid?q=covid&lang=en&media=True",
-    options
-)
-    .then((response) => {
-        return response.json();
-    })
-    //concating all news headings in a single div
-    .then((response) => {
-        let articles = response.articles.slice(10,30);
-        for(news in articles){
-            console.log();
-            let headings=`
-                <div>${articles[news].title}</div>
-            `
-            newsDisplay+=headings;
-        }
-        newsList.innerHTML=newsDisplay;
-    })
+// fetch(
+//     "https://covid-19-news.p.rapidapi.com/v1/covid?q=covid&lang=en&media=True",
+//     options
+// )
+//     .then((response) => {
+//         return response.json();
+//     })
+//     //concating all news headings in a single div
+//     .then((response) => {
+//         let articles = response.articles.slice(10,30);
+//         for(news in articles){
+//             console.log();
+//             let headings=`
+//                 <div>${articles[news].title}</div>
+//             `
+//             newsDisplay+=headings;
+//         }
+//         newsList.innerHTML=newsDisplay;
+//     })
 
-//if error happens
-    .catch((err) => console.error(err));
+// //if error happens
+//     .catch((err) => console.error(err));
 
 
 //adding dark mode using jquery
