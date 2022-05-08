84cc895c802e4666672513d78cb6c3b99376e872
Manas Ranjan Manna <madanmanna123456@gmail.com>
2022-05-08 13:08:55 +0530

    Final commit

---
+++ Detection App/README.md
@@ -0,0 +1,4 @@
+TensorFlow Object Detection Web Application
+To run this 
+1. Install App Depdendencies -> npm install
+2. Start the app by running npm start -> npm start
---
+++ Detection App/src/utilities.js
@@ -0,0 +1,30 @@
+// Define our labelmap
+const labelMap = {
+    1:{name:'FirstDose', color:'yellow'},
+    2:{name:'SecondDose', color:'green'},
+    3:{name:'NotVaccinated', color:'red'},
+
+}
+
+// Define a drawing function
+export const drawRect = (boxes, classes, scores, threshold, imgWidth, imgHeight, ctx)=>{
+    for(let i=0; i<=boxes.length; i++){
+        if(boxes[i] && classes[i] && scores[i]>threshold){
+            // Extract variables
+            const [y,x,height,width] = boxes[i]
+            const text = classes[i]
+            
+            // Set styling
+            ctx.strokeStyle = labelMap[text]['color']
+            ctx.lineWidth = 10
+            ctx.fillStyle = 'white'
+            ctx.font = '30px Arial'         
+            
+            // DRAW!!
+            ctx.beginPath()
+            ctx.fillText(labelMap[text]['name'] + ' - ' + Math.round(scores[i]*100)/100, x*imgWidth, y*imgHeight-10)
+            ctx.rect(x*imgWidth, y*imgHeight, width*imgWidth/2, height*imgHeight/2);
+            ctx.stroke()
+        }
+    }
+}
